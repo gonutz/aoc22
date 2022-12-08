24d9124da3dbb1cf7c767f2210750bda095b6c76
gonutz <gonutz@fake.mail>
2022-12-08 20:10:19 +0100

    Improve algorithm runtime from O(N*M) to O(N)
    Where N is the input string length and M is the number of distinct
    elements that we look for.

--- day6/main.go
+++ day6/main.go
@@ -4,24 +4,58 @@ import "fmt"
 
 func main() {
 	firstDistinct := func(n int) int {
+		// counts is indexed by ASCII letter, e.g. counts['a'] holds the number
+		// of 'a's in the currently looked at n-character sequence.
 		var counts [128]int
-		for i := range input {
-			for j := 0; j < n; j++ {
-				counts[input[i+j]]++
-			}
 
-			allOK := true
-			for j := 0; j < n; j++ {
-				allOK = allOK && counts[input[i+j]] == 1
-			}
-			if allOK {
-				return i + n
-			}
+		// Gather the first n counts.
+		for i := 0; i < n; i++ {
+			counts[input[i]]++
+		}
 
-			for j := 0; j < n; j++ {
-				counts[input[i+j]]--
+		// Count how many of these counts are 1.
+		ones := 0
+		for _, n := range counts {
+			if n == 1 {
+				ones++
 			}
 		}
+
+		// In case we have all 1s already, we are golden.
+		if ones == n {
+			return n
+		}
+
+		// Move our sliding window over by 1 in each iteration.
+		// We remove the left-most character from our counts and add the new
+		// letter on the right to our counts.
+		// Then we check again whether all counts are 1. Instead of looping over
+		// the counts array every time to check this, we update the ones
+		// variable. This saves time.
+		update := func(c byte, countDelta int) {
+			if counts[c] == 1 {
+				ones--
+			}
+
+			counts[c] += countDelta
+
+			if counts[c] == 1 {
+				ones++
+			}
+		}
+		for i := n; i < len(input); i++ {
+			oldest := input[i-n]
+			newest := input[i]
+
+			update(oldest, -1)
+			update(newest, +1)
+
+			if ones == n {
+				return i + 1
+			}
+		}
+
+		// If we end here, nothing was found.
 		return -1
 	}
 
